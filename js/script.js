be7d9056e7e1bc381c1167c9e950bfd33f718d6f
Kathrin <kathrin@Kathrins-Air.homebase>
2018-04-28 16:34:34 +0200

    updatefiles

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function calculateInsurance() {
+
+    var name = document.getElementById("fullname").value;
+    var age = parseInt(document.getElementById("age").value);
+    var power = parseInt(document.getElementById("hp").value);
+    var location = document.getElementById("countryselect").value;
+    var car = document.getElementById("modelselect").value;
+
+    if (age >= 85 || age <=17) {
+        document.getElementById("output").innerHTML = "Dear " + name + ", sorry we can not insure your car.";
+    } else if (power >= 180 || power <= 44) {
+        document.getElementById("output").innerHTML = "Dear " + name + ", sorry we can not insure your car.";
+    } else if (location == "Austria") {
+        result = (((power * 100) / age) + 50);
+        document.getElementById("output").innerHTML = "Dear " + name + ", your insurance costs are " + Math.round(result) + " €. <br>Because of your " + power + " kW HP car model " + car + " and your age of " + age + ".";
+    } else if (location == "Germany") {
+        result = (((power * 120) / age) + 100);
+        document.getElementById("output").innerHTML = "Dear " + name + ", your insurance costs are " + Math.round(result) + " €. <br>Because of your " + power + " kW HP car model " + car + " and your age of " + age + ".";
+    } else if (location == "Italy") {
+        result = (((power * 150) / (age + 2)) + 100);
+        document.getElementById("output").innerHTML = "Dear " + name + ", your insurance costs are " + Math.round(result) + " €. <br>Because of your " + power + " kW HP car model " + car + " and your age of " + age + ".";
+    }
+};
